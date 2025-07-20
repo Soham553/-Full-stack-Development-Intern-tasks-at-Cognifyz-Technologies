944433d38a5e582b64c20884b9a171575b3eb751
Soham553 <sakharesoham10@gmail.com>
2025-07-20 20:00:00 +0530

    Rendering login message on web page

--- Server_side_rendering/frontend/main.js
+++ Server_side_rendering/frontend/main.js
@@ -2,28 +2,40 @@ console.log("hello this is just starting");
 
 let subBtn = document.getElementById('form');
 
-
 subBtn.addEventListener("submit", async (e) => {
     e.preventDefault();
     const fromData = new FormData(subBtn);
     const userName = fromData.get('userName');
     const email = fromData.get('email');
-    const phone_No = fromData.get('phone_NO');
+    const phone_No = fromData.get('phone_No');
+
     const data = Object.fromEntries(fromData);
 
-    try {
-        const resp = await fetch('http://localhost:3000/submit', {
-                        method : "POST",
-                        headers : {
-                            "Content-Type": "application/json"
-                        },
-                        body : JSON.stringify(data)
-                    })
-        
-        
-    } catch (error) {
-        console.log(error);
+    if (!userName || !email || !phone_No) {
+        alert("Fill all the information");
+        return;
     }
-})
 
+    try {
+        const resp = await fetch('http://localhost:3000', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
 
+        const result = await resp.text();
+        let newdiv = document.createElement('div');
+        newdiv.append(result);
+        subBtn.style.display = 'none';
+        document.body.appendChild(newdiv);
+        newdiv.style.display = "flex";
+        newdiv.style.alignItems = "center";
+        newdiv.style.justifyContent = "left-50vh";
+        newdiv.style.color = "green";
+
+    } catch (error) {
+        console.log("Fetch error:", error);
+    }
+});
--- Server_side_rendering/index.js
+++ Server_side_rendering/index.js
@@ -3,31 +3,25 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
- app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.set('view engine', 'ejs');
 
-
 app.set('views', path.join(__dirname, 'frontend'));
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.render('html');
+  res.render('index');
 });
 
-
-app.post('/submit',  (req, res) => {
-  console.log(req.body);
-  res.send("hello");
+app.post('/',  (req, res) => {
+  console.log(req.body)
+  res.send("You have login successfuly");
 })
-
-
 
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000/');
